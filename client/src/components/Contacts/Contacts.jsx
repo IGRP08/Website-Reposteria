81f7246707f7a2e9b5f34d1c7a09b1280b4c3d24
igrp <ivangabrielromano@hotmail.com>
2026-02-07 13:25:08 -0400

    Fixed Contacts

--- client/src/components/Contacts/Contacts.jsx
+++ client/src/components/Contacts/Contacts.jsx
@@ -1,61 +1,8 @@
 import React from "react";
-import './contacts.css'
+import './Contacts.css'
 
 
 
-// const SocialButtons = () => {
-//     const openLink = (URL) => {
-//         window.open(URL, "_blank")
-//     };
-
-//     // Whatsapp Button Constants
-
-//     const whatsappNumber = "18098709330"
-//     const whatMessage = "Hola, vengo de su página web y quería saber más acerca de sus servicios."
-//     const whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatMessage)}`
-
-//     // Instagram Button Constants
-
-//     const instagramURL = "https://www.instagram.com/makariospostres?igsh=ZHJtenpmbnRvaDg4"
-
-//     // Facebook Button Constants
-
-//     const facebookURL = "https://www.facebook.com/share/15wMEGjzjq/"
-
-//     return (
-
-//         <div className={`social-buttons-container ${className}`}>
-
-//             {/* Whatsapp Button */}
-
-//             <button className="social-button" onClick={() => openLink(whatsappURL)}>
-
-//                 <img src="/assets/Whatsapp_icon-color.png" alt="Whatsapp" />
-//             </button>
-
-//             {/* Instagram Button */}
-
-//             <button className="social-button" onClick={() => openLink(instagramURL)}>
-
-//                 <img src="/assets/instagram_icon_color.png" alt="Instagram" />
-//             </button>
-
-//             {/* Facebook Button */}
-
-//             <button className="social-button" onClick={() => openLink(facebookURL)}>
-
-//                 <img src="/assets/facebook_icon-color.png" alt="Facebook" />
-//             </button>
-
-//         </div>
-//     );
-// };
-
-// export default SocialButtons;
-
-// src/components/Contacts/Contacts.jsx
-
-/* 1. Add { className } here to receive the props from App.jsx */
 
 
 const Contacts = ({ className }) => {
