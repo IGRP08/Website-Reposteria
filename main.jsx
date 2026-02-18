ae5bcb21003db49200afb64e977a91ac3c11d6f5
igrp <ivangabrielromano@hotmail.com>
2026-02-18 11:21:51 -0400

    I changed the path of main.jsx to root folder in the index.html file.

--- main.jsx
+++ main.jsx
@@ -1,37 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from '/App.jsx'
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App.jsx';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<App />);
-
-
-// main.jsx
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App.jsx'; // FIX: Removed leading slash
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
