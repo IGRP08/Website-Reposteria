a4b343587fcb2f739ecd1037d07ec72c389168f6
igrp <ivangabrielromano@hotmail.com>
2026-02-18 16:58:42 -0400

    Made changes to the static.yml and vite.config.js files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig(({ mode }) => {
+
+//   return {
+//     base: mode === 'production' ? '/Website-Reposteria/' : '/',
+//     plugins: [react()],
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-
-  return {
-    base: mode === 'production' ? '/Website-Reposteria/' : '/',
-    plugins: [react()],
-  }
+export default defineConfig({
+  // The base must match your repository name exactly
+  base: '/Website-Reposteria/',
+  plugins: [react()],
 })
